a2628684ac86cc3237d76bcc0872c27edc242bf9
WardM99 <wardmeersman@gmail.com>
2023-10-05 19:08:01 +0200

    :test_tube: BST contains tests

--- my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
+++ my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
@@ -1,5 +1,10 @@
 package com.mycompany.app.binarysearchtree;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.NoSuchElementException;
+
 import org.junit.Test;
 
 public class BSTTest {
@@ -23,5 +28,27 @@ public class BSTTest {
         BSTInterface<Integer> b = new BST<Integer>();
         b.add(5);
         b.add(5);
+    }
+    @Test
+    public void testContains(){
+        BSTInterface<Integer> b = new BST<Integer>();
+        assertFalse(b.contains(5));
+        b.add(5);
+        assertTrue(b.contains(5));
+        b.add(1);
+        assertTrue(b.contains(1));
+        b.add(10);
+        assertTrue(b.contains(10));
+        b.add(6);
+        assertTrue(b.contains(6));
+    }
+    @Test
+    public void testDontContains(){
+        BSTInterface<Integer> b = new BST<Integer>();
+        b.add(5);
+        b.add(1);
+        b.add(10);
+        b.add(6);
+        assertFalse(b.contains(9));
     }
 }
