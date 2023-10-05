72314e9720f87d4109e1ac193fd412a283597cbe
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2023-10-05 18:45:10 +0200

    ✨ BST add (#23)
    * :test_tube: added test for BST add
    
    * :white_mark_check: BST add

--- my-app/src/main/java/com/mycompany/app/binarysearchtree/BST.java
+++ my-app/src/main/java/com/mycompany/app/binarysearchtree/BST.java
@@ -1,5 +1,54 @@
 package com.mycompany.app.binarysearchtree;
 
-public class BST {
+public class BST<T> implements BSTInterface<T>{
+    private BSTNodeInterface<T> root;
+
+    BST(){
+        this.root = null;
+    }
+
+    @Override
+    public void add(Comparable<T> value) {
+        T valueUnpacked = (T) value;
+        if(this.root == null){
+            this.root = new BSTNode<T>(value);
+        } 
+        else {
+            BSTNodeInterface<T> current = this.root;
+            BSTNodeInterface<T> prev = this.root;
+            while(current != null){
+                prev = current;
+                Comparable<T> currentValue = current.getValue();
+                if(currentValue.compareTo(valueUnpacked) < 0){ //left child
+                    current = current.getLeftChild();
+                }
+                else if(currentValue.compareTo(valueUnpacked) > 0){ //right child
+                    current = current.getRightChild();
+                }
+                else { // same value
+                    throw new IllegalArgumentException("Value already in tree");
+                }
+            }
+            Comparable<T> prevValue = prev.getValue();
+            if(prevValue.compareTo(valueUnpacked) < 0){ //left child
+                prev.setLeftChild(new BSTNode<T>(value));
+            }
+            else if(prevValue.compareTo(valueUnpacked) > 0){ //right child
+                prev.setRightChild(new BSTNode<T>(value));
+            }
+        }
+    }
+
+    @Override
+    public boolean contains(Comparable<T> value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public void remove(Comparable<T> value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
     
 }
--- my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
+++ my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
@@ -1,5 +1,27 @@
 package com.mycompany.app.binarysearchtree;
 
+import org.junit.Test;
+
 public class BSTTest {
-    
+    @Test
+    public void makeBST(){
+        new BST<Integer>();
+    }
+    @Test 
+    public void testAdd(){
+        BSTInterface<Integer> b = new BST<Integer>();
+        b.add(5);
+    }
+    @Test 
+    public void testAddMultiple(){
+        BSTInterface<Integer> b = new BST<Integer>();
+        b.add(5);
+        b.add(6);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddDublicate(){
+        BSTInterface<Integer> b = new BST<Integer>();
+        b.add(5);
+        b.add(5);
+    }
 }
