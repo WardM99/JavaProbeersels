aa38cfceb503a4c9f8a2961a4e36e7eb5409c5f3
WardM99 <wardmeersman@gmail.com>
2023-10-05 19:11:23 +0200

    :white_check_mark: BST contains

--- my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
+++ my-app/src/test/java/com/mycompany/app/binarysearchtree/BSTTest.java
@@ -3,8 +3,6 @@ package com.mycompany.app.binarysearchtree;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.NoSuchElementException;
-
 import org.junit.Test;
 
 public class BSTTest {
