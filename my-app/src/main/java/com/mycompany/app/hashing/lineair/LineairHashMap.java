975f976b6bab8ea8fed37cd76a66ad6e77c30a6f
WardM99 <wardmeersman@gmail.com>
2023-10-09 16:55:24 +0200

    :sparkles: structure of Lineair Hashing

---
+++ my-app/src/main/java/com/mycompany/app/hashing/lineair/LineairHashMap.java
@@ -0,0 +1,45 @@
+package com.mycompany.app.hashing.lineair;
+
+import java.util.NoSuchElementException;
+
+import com.mycompany.app.hashing.HashmapInterface;
+
+public class LineairHashMap<K, V> implements HashmapInterface<K, V> {
+
+    @Override
+    public void put(K key, V value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'put'");
+    }
+
+    @Override
+    public V get(K key) throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public V getOrDefault(K key, V defaultValue) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOrDefault'");
+    }
+
+    @Override
+    public void remove(K key) throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public void remove(K Key, V value) throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'containsKey'");
+    }
+    
+}
