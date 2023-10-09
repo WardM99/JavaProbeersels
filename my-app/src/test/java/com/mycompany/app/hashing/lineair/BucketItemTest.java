7f05b4652a904cb597b7043fe62f4fccb5146372
WardM99 <wardmeersman@gmail.com>
2023-10-09 20:55:15 +0200

    :sparkles: made BucketItem

---
+++ my-app/src/test/java/com/mycompany/app/hashing/lineair/BucketItemTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.app.hashing.lineair;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class BucketItemTest {
+    @Test
+    public void makeBucketItem(){
+        new BucketItem<String,Integer>("TEST", 5);
+    }
+    @Test
+    public void equalsBucketItems(){
+        BucketItem<String, Integer> bi1 = new BucketItem<String,Integer>("TEST", 5);
+        BucketItem<String, Integer> bi2 = new BucketItem<String,Integer>("TEST", 6);
+        BucketItem<String, Integer> bi3 = new BucketItem<String,Integer>("LEST", 5);
+        BucketItem<String, Integer> bi4 = new BucketItem<String,Integer>("LEST", 6);
+        BucketItem<String, Integer> bi5 = new BucketItem<String,Integer>("TEST", 5);
+        assertFalse(bi1.equals(bi2));
+        assertFalse(bi1.equals(bi3));
+        assertFalse(bi1.equals(bi4));
+        assertTrue(bi1.equals(bi5));
+    }
+}
