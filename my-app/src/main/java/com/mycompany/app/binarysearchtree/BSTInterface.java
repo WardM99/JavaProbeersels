1163e10e0b3541c0681c86f4651db4d248c785a9
WardM99 <wardmeersman@gmail.com>
2023-10-09 13:17:27 +0200

    :white_check_mark: BST remove

--- my-app/src/main/java/com/mycompany/app/binarysearchtree/BSTInterface.java
+++ my-app/src/main/java/com/mycompany/app/binarysearchtree/BSTInterface.java
@@ -1,7 +1,9 @@
 package com.mycompany.app.binarysearchtree;
 
+import java.util.NoSuchElementException;
+
 public interface BSTInterface<T> {
-    public void add(Comparable<T> value);
+    public void add(Comparable<T> value) throws IllegalArgumentException;
     public boolean contains(Comparable<T> value);
-    public void remove(Comparable<T> value); 
+    public void remove(Comparable<T> value) throws NoSuchElementException; 
 }
