2578bb4552bbc99a89c76845f5e8bec51cd48310
WardM99 <wardmeersman@gmail.com>
2023-10-27 21:21:16 +0200

    :sparkles: queue

---
+++ my-app/src/main/java/com/mycompany/app/queue/Queue.java
@@ -0,0 +1,23 @@
+package com.mycompany.app.queue;
+
+public class Queue<T> implements QueueInterface<T> {
+
+    @Override
+    public void enqueue(T value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+    
+}
