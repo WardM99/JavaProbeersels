ae309d832c5117d4d2a5c11817347a5a948a9e47
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2023-10-27 17:00:29 +0200

    ✨ implements stack (#62)
    * :bookmark: update version tag
    
    * :sparkles: stack
    
    * :test_tube: added stack tests
    
    * :white_check_mark: tests pass
    
    * :bookmark: set snapshot version
    
    * :white_check_mark: added more tests
    
    * :recycle: fixes code style error

---
+++ my-app/src/main/java/com/mycompany/app/stack/Stack.java
@@ -0,0 +1,39 @@
+package com.mycompany.app.stack;
+
+import java.util.EmptyStackException;
+
+import com.mycompany.app.linkedlist.Node;
+
+public class Stack<T> implements StackInterface<T> {
+
+    Node<T> head;
+
+    public Stack(){
+        this.head = null;
+    }
+
+    @Override
+    public void push(T value) {
+        Node<T> newNode = new Node<T>(value);
+        newNode.setNextNode(this.head);
+        this.head = newNode;
+    }
+    
+    @Override
+    public T pop() throws EmptyStackException {
+        if(this.head == null)
+            throw new EmptyStackException();
+
+        T value = this.head.getValue();
+        this.head = this.head.getNextNode();
+        return value;
+    }
+
+    @Override
+    public T peek() {
+        if(this.head == null)
+            return null;
+        return this.head.getValue();
+    }
+    
+}
---
+++ my-app/src/test/java/com/mycompany/app/stack/StackTest.java
@@ -0,0 +1,58 @@
+package com.mycompany.app.stack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.EmptyStackException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackTest {
+    private Stack<Integer> s;
+
+    @Before
+    public void setUp() {
+        s = new Stack<>();
+    }
+    
+    @Test
+    public void basicTests(){
+        s.push(5);
+        s.push(6);
+
+        int valuePeek = s.peek();
+        int valuePop = s.pop();
+
+        assertEquals(6, valuePeek);
+        assertEquals(6, valuePop);
+
+        assertTrue(valuePeek == valuePop);
+
+        valuePeek = s.peek();
+        assertEquals(5, valuePeek);
+    }
+
+    @Test
+    public void peekEmptyStackTest(){
+        Integer valuePeek = s.peek();
+
+        assertNull(valuePeek);
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void popEmptyStackTest(){
+        s.pop();
+    }
+    
+    @Test
+    public void testPushAndPopMixed() {
+        s.push(100);
+        s.push(200);
+        assertEquals(200, s.pop().intValue());
+        s.push(300);
+        assertEquals(300, s.pop().intValue());
+        assertEquals(100, s.pop().intValue());
+    }
+}
