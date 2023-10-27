21fb4e35121435be00c21b125053dad6d68f2159
WardM99 <wardmeersman@gmail.com>
2023-10-27 16:15:57 +0200

    :test_tube: added stack tests

---
+++ my-app/src/test/java/com/mycompany/app/stack/StackTest.java
@@ -0,0 +1,42 @@
+package com.mycompany.app.stack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.EmptyStackException;
+
+import org.junit.Test;
+
+public class StackTest {
+    @Test
+    public void basicTests(){
+        StackInterface<Integer> s = new Stack<Integer>();
+        s.push(5);
+        s.push(6);
+
+        int valuePeek = s.peek();
+        int valuePop = s.pop();
+
+        assertEquals(5, valuePeek);
+        assertEquals(5, valuePop);
+
+        assertTrue(valuePeek == valuePop);
+
+        valuePeek = s.peek();
+        assertEquals(6, valuePeek);
+    }
+
+    @Test
+    public void peekEmptyStackTest(){
+        StackInterface<Integer> s = new Stack<Integer>();
+        Integer valuePeek = s.peek();
+
+        assertNull(valuePeek);
+    }
+    @Test(expected = EmptyStackException.class)
+    public void popEmptyStackTest(){
+        StackInterface<Integer> s = new Stack<Integer>();
+        s.peek();
+    }
+}
