c8c9a9375dd8a1a3347b1e422e78b193bba99c60
WardM99 <wardmeersman@gmail.com>
2023-10-27 16:37:13 +0200

    :white_check_mark: tests pass

--- my-app/src/main/java/com/mycompany/app/stack/Stack.java
+++ my-app/src/main/java/com/mycompany/app/stack/Stack.java
@@ -1,23 +1,38 @@
 package com.mycompany.app.stack;
 
+import java.util.EmptyStackException;
+
+import com.mycompany.app.linkedlist.Node;
+
 public class Stack<T> implements StackInterface<T> {
 
-    @Override
-    public void push(T value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    Node<T> head;
+
+    public Stack(){
+        this.head = null;
     }
 
     @Override
-    public T pop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    public void push(T value) {
+        Node<T> newNode = new Node<T>(value);
+        newNode.setNextNode(this.head);
+        this.head = newNode;
+    }
+    @Override
+    public T pop() throws EmptyStackException {
+        if(this.head == null)
+            throw new EmptyStackException();
+        
+        T value = this.head.getValue();
+        this.head = this.head.getNextNode();
+        return value;
     }
 
     @Override
     public T peek() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+        if(this.head == null)
+            return null;
+        return this.head.getValue();
     }
     
 }
